b39dc1dbc0b9fb6f4577fa7bf98a20d99fffe206
2ono <clt3555@gmail.com>
2023-05-21 23:34:23 +0900

    [add] delete rows in shopping_cart table

--- back-end/OrderItem/src/Order.java
+++ back-end/OrderItem/src/Order.java
@@ -5,8 +5,10 @@ import java.sql.SQLException;
 
 public class Order {
 	enum Choose {
-		ITEMLIST(1), BUYITEM(2), CANCELBUY(3), EXITSHOP(4);
+		ITEMLIST(1), BUYITEM(2), SHOPPINGLIST(3), CANCELBUY(4), EXITSHOP(5);
+
 		private int value;
+
 		private Choose(int value) {
 			this.value = value;
 		}
@@ -14,8 +16,11 @@ public class Order {
 
 	private static final int ITEMLIST = 1;
 	private static final int BUYITEM = 2;
+	private static final int SHOPPINGLIST = 3;
+	private static final int CANCELBUY = 4;
 
-	public static void main(String[] args) throws NumberFormatException, IOException, SQLException, ClassNotFoundException {
+	public static void main(String[] args)
+			throws NumberFormatException, IOException, SQLException, ClassNotFoundException {
 		System.out.println("온라인 쇼핑몰에 오신걸 환영합니다");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -26,9 +31,7 @@ public class Order {
 			System.out.println("4번 누르면 장바구니 비우기를 합니다.");
 			System.out.println("5번 누르면 온라인쇼핑몰에서 나가집니다.");
 			int c = Integer.parseInt(br.readLine());
-			
-			
-			
+
 			if (c == ITEMLIST) {
 				OrderDao.showAllItem();
 			} else if (c == BUYITEM) {
@@ -36,25 +39,26 @@ public class Order {
 				String name = br.readLine();
 				System.out.println("원하는 아이템 가격을 입력하세요");
 				int price = Integer.parseInt(br.readLine());
-				
-				
-				
+
 				OrderItem oi = new OrderItem(name, price);
 				boolean answer = OrderDao.insertItemToDB(oi);
-			
+
 				System.out.println(oi);
-				
-				if(answer) {
+
+				if (answer) {
 					System.out.println("성공적으로 쇼핑카트에 집어 넣었습니다");
-				}else {
+				} else {
 					System.out.println("뭔가 에러가 발생했습니다.....");
 				}
-			} else if (c == 3) {
+			} else if (c == SHOPPINGLIST) {
 				OrderDao.showShoppingList();
-			} else if (c == 4) {
-
-			}
-			else if (c == 5) {
+				
+				
+			} else if (c == CANCELBUY) {
+				OrderDao.deleteShoppingDB();
+				System.out.println("성공적으로 쇼핑카트가 비워집니다");
+				System.out.println("=========================");
+			} else if (c == 5) {
 				break;
 			} else {
 				throw new NumberFormatException();
@@ -62,7 +66,5 @@ public class Order {
 
 		}
 	}
-
-	
 
 }
--- back-end/OrderItem/src/OrderDao.java
+++ back-end/OrderItem/src/OrderDao.java
@@ -6,26 +6,26 @@ import java.sql.Statement;
 
 public class OrderDao {
 	private static Connection con = null;
-	
+
 	public static void showAllItem() throws SQLException {
 //		boolean = f = false;
-		
+
 		try {
 			Connection con = JDBC.createC();
 			// 상품 검색
 			String q = "select * from goods;";
-			
+
 			Statement stmt = con.createStatement();
 			stmt.executeQuery(q);
-			
+
 			java.sql.ResultSet set = stmt.executeQuery(q);
-			
-			while(set.next()) {
+
+			while (set.next()) {
 				int id = set.getInt(1);
 				String name = set.getString(2);
 				String info = set.getString(3);
 				int price = set.getInt(4);
-				
+
 				System.out.println("ID : " + id);
 				System.out.println("Item Name : " + name);
 				System.out.println("Item Info : " + name);
@@ -36,13 +36,14 @@ public class OrderDao {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			
+
 			// try-with-resources 추가
 		} finally {
-			 if (con != null) con.close();
+			if (con != null)
+				con.close();
 		}
 	}
-	
+
 	public static boolean insertItemToDB(OrderItem oi) throws ClassNotFoundException, IOException, SQLException {
 
 		// jdbc code...
@@ -66,30 +67,31 @@ public class OrderDao {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			 if (con != null) con.close();
+			if (con != null)
+				con.close();
 		}
 		return f;
 
 	}
-	
-	
+
+	// 장바구니 내용 검색
 	public static void showShoppingList() throws SQLException {
 //		boolean = f = false;
-		
+
 		try {
 			Connection con = JDBC.createC();
 			// 상품 검색
 			String q = "select * from shopping_cart;";
-			
+
 			Statement stmt = con.createStatement();
 			stmt.executeQuery(q);
-			
+
 			java.sql.ResultSet set = stmt.executeQuery(q);
-			
-			while(set.next()) {
+
+			while (set.next()) {
 				String name = set.getString(1);
 				int price = set.getInt(2);
-				
+
 				System.out.println("Item Name : " + name);
 				System.out.println("Price : " + price);
 				System.out.println("==========================");
@@ -98,10 +100,33 @@ public class OrderDao {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			
+
 			// try-with-resources 추가
 		} finally {
+			if (con != null)
+				con.close();
+		}
+	}
+
+	// 장바구니 내용 비움
+	public static boolean deleteShoppingDB() throws SQLException {
+		boolean f = false;
+		try {
+			Connection con = JDBC.createC();
+			String q = "truncate shopping_cart;";
+			
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate(q);
+			
+			f = true;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
 			 if (con != null) con.close();
 		}
+		return f = false;
+		
 	}
 }
